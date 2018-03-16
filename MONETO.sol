e2784fa168d9d260c72c63071e6ecb3e62837a5d
Albert Ibragimov <a@prosto-a.ru>
2018-03-16 11:56:25 +0300

    add Token and PreSale contracts

---
+++ MONETO.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.12;
+
+import "./lib/BurnableToken.sol";
+import "./lib/UpgradeableToken.sol";
+
+contract MONETO is BurnableToken, UpgradeableToken {
+
+  string public name;
+  string public symbol;
+  uint public decimals;
+
+  function MONETO(address _owner)  UpgradeableToken(_owner) {
+    name = "MONETO";
+    symbol = "MTO";
+    totalSupply = 3950000000000000000000000;
+    decimals = 18;
+
+    balances[_owner] = totalSupply;
+  }
+}
